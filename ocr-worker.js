41df8510fbad7c37aeb4f5c754ccaa3f23071070
8zt7pgb246-coder <8zt7pgb246@privaterelay.appleid.com>
2026-08-19 03:54:19 +0900

    Add files via upload

--- ocr-worker.js
+++ ocr-worker.js
@@ -1,2 +1,2 @@
-/* v47 same-origin bootstrap for Tesseract.js on GitHub Pages / iPhone Safari. */
+/* Same-origin bootstrap for Tesseract.js on GitHub Pages / iPhone Safari. */
 importScripts('https://cdn.jsdelivr.net/npm/tesseract.js@5.1.1/dist/worker.min.js');

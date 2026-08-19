676826718561394dc501163cc0b664b8724b4097
8zt7pgb246-coder <8zt7pgb246@privaterelay.appleid.com>
2026-08-19 10:00:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* xm-losscut v56: no application caching. Network-first and never stores HTML/JS. */
+/* xm-losscut v57: no application caching. Network-first and never stores HTML/JS. */
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
